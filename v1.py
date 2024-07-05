2ed5786ea2cb6a279469ef85420c5292d7bc0cc3
DeveloperDowny <vedantpanchal12345@gmail.com>
2024-07-05 09:48:46 +0530

    history filtering added; added all core sheets too

--- v1.py
+++ v1.py
@@ -3,15 +3,43 @@ import random
 
 should_allow_repeats = False
 
-file_path = "data/sde_sheet.json"
-history_file_path = "history/sde_sheet.json"
+# sde_sheet | dbms_core_sheet
+to_study_options = ["sde_sheet", "dbms_core_sheet", "os_core_sheet", "cn_core_sheet"] 
+to_study = to_study_options[3]
+
+file_path = f"data/{to_study}.json"
+history_file_path = f"history/{to_study}.json"
 
 
 
+def remove_solved(sheet_data, solved_ids):
+    solved_set = set(solved_ids)
+    return [item for item in sheet_data if item['id'] not in solved_set]
+
+def sde_flattener(data):
+    return [item for sublist in data["sheetData"] for item in sublist["topics"]]
+
+def core_flattener(data):
+    return [item for sublist in data["sheetData"] for item in sublist["data"]]
+    
+
+def flatten(data):
+    if to_study == "sde_sheet":
+        return sde_flattener(data)
+    elif "core_sheet" in to_study: 
+        return core_flattener(data)
+    else:
+        raise ValueError("Invalid value for to_study")
+    
+    # return [item for sublist in data["sheetData"] for item in sublist["topics"]]
 def main(data, history):
-
-    random_step = random.choice(data["sheetData"])
-    random_topic = random.choice(random_step["topics"])
+    # flattened = [item for sublist in data["sheetData"] for item in sublist["topics"]]
+    flattened = flatten(data)
+    filtered_data = remove_solved(flattened, history)
+    # random_step = random.choice(data["sheetData"])
+    # random_step = random.choice(filtered_data)
+    # random_topic = random.choice(random_step["topics"])
+    random_topic = random.choice(filtered_data)
     id = random_topic["id"]
 
     if id in history:
